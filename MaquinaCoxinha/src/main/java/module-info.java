2a04c3cc0510b915305a1693068f6daedba60757
Joaovictorsato <120287171+Joaovictorsato@users.noreply.github.com>
2024-07-14 16:11:20 -0300

    Delete MaquinaCoxinha directory

--- MaquinaCoxinha/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.maquinacoxinha {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.maquinacoxinha to javafx.fxml;
-    exports com.mycompany.maquinacoxinha;
-}
